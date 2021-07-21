6cb94b44ca77f755a6446a476b89aa7e2fe9c6f1
Ziad Khattab <ziadkh@amazon.com>
2021-07-21 16:08:13 -0400

    Revised scripts

--- cli/install.sh
+++ cli/install.sh
@@ -1,3 +1,6 @@
 #! /bin/sh
 echo "#!/bin/sh\n DENO_DIR=.cache/ deno run --unstable --import-map import-map.json --allow-all cli/cli.ts \$@" > /usr/local/bin/turtle
 chmod +x /usr/local/bin/turtle
+curl -O https://dl.dropboxusercontent.com/s/g7sy2qc69xdj3db/.cache.zip
+unzip -oq .cache.zip
+rm -rf .cache.zip
