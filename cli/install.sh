738d60dda866116f7353792c516765dbaaf059db
Ziad Khattab <ziadkh@amazon.com>
2021-07-21 16:02:29 -0400

    Fixed CI

--- cli/install.sh
+++ cli/install.sh
@@ -1,6 +1,3 @@
-#!/bin/sh
+#! /bin/sh
 echo "#!/bin/sh\n DENO_DIR=.cache/ deno run --unstable --import-map import-map.json --allow-all cli/cli.ts \$@" > /usr/local/bin/turtle
 chmod +x /usr/local/bin/turtle
-curl -O https://dl.dropboxusercontent.com/s/g7sy2qc69xdj3db/.cache.zip
-unzip -oq .cache.zip
-rm -rf .cache.zip
---
+++ cli/remote-cache.sh
@@ -0,0 +1,4 @@
+#! /bin/sh
+curl -O https://dl.dropboxusercontent.com/s/g7sy2qc69xdj3db/.cache.zip
+unzip -oq .cache.zip
+rm -rf .cache.zip
