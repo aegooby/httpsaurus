6018e3f04926ec56d276f5964e96fe1fdc9f3336
Ziad Khattab <ziadkh@amazon.com>
2021-07-22 14:36:22 -0400

    Moved listener to separate file

--- server/server.tsx
+++ server/server.tsx
@@ -13,6 +13,8 @@ import * as jwt from "jsonwebtoken";
 import * as keypair from "keypair";
 
 import { GraphQL } from "./graphql.tsx";
+import { Listener } from "./listener.tsx";
+import type { ListenOptions, ListenBaseOptions, ListenTlsOptions } from "./listener.tsx";
 import { Console } from "./console.tsx";
 export { Console } from "./console.tsx";
 
@@ -34,115 +36,6 @@ class Version
     }
 }
 export const version: Version = new Version(2, 1, 0);
-
-interface ListenBaseOptions extends Deno.ListenOptions
-{
-    secure: false;
-}
-interface ListenTlsOptions extends Deno.ListenTlsOptions
-{
-    secure: true;
-}
-type ListenOptions = ListenBaseOptions | ListenTlsOptions;
-type ConnectionAsyncIter =
-    {
-        [Symbol.asyncIterator](): AsyncGenerator<Deno.Conn, never, unknown>;
-    };
-class Listener
-{
-    private nativeListeners: Map<number, [boolean, Deno.Listener]> = new Map<number, [boolean, Deno.Listener]>();
-    private options: Array<ListenOptions> = [];
-
-    constructor(options?: Array<ListenOptions>)
-    {
-        if (options) this.options = options;
-
-        this.create = this.create.bind(this);
-        this.listen = this.listen.bind(this);
-        this.connections = this.connections.bind(this);
-        this.secure = this.secure.bind(this);
-        this.listener = this.listener.bind(this);
-        this.keys = this.keys.bind(this);
-        this.close = this.close.bind(this);
-    }
-    private create(options: ListenOptions): [boolean, Deno.Listener]
-    {
-        if (options.secure)
-        {
-            const listener = Deno.listenTls(options as Deno.ListenTlsOptions);
-            this.nativeListeners.set(listener.rid, [options.secure, listener]);
-            return [options.secure, listener];
-        }
-        else
-        {
-            const listener = Deno.listen(options as Deno.ListenOptions);
-            this.nativeListeners.set(listener.rid, [options.secure, listener]);
-            return [options.secure, listener];
-        }
-    }
-    public listen(options?: Array<ListenOptions>): Array<[boolean, Deno.Listener]>
-    {
-        if (!options)
-            return this.options.map(this.create);
-        else
-            return options.map(this.create);
-    }
-    public connections(key: number): ConnectionAsyncIter
-    {
-        if (!this.nativeListeners.has(key))
-            throw new Error("Listener not found");
-        const [_, nativeListener] = this.nativeListeners.get(key) as [boolean, Deno.Listener];
-        const iterable =
-        {
-            async *[Symbol.asyncIterator]()
-            {
-                while (true)
-                {
-                    try
-                    {
-                        const connection = await nativeListener.accept();
-                        yield connection;
-                    }
-                    catch { undefined; }
-                }
-            }
-        };
-        return iterable;
-    }
-    public secure(key: number): boolean
-    {
-        if (!this.nativeListeners.has(key))
-            throw new Error("Listener not found");
-        const [secure, _] = this.nativeListeners.get(key) as [boolean, Deno.Listener];
-        return secure;
-    }
-    public listener(key: number): Deno.Listener
-    {
-        if (!this.nativeListeners.has(key))
-            throw new Error("Listener not found");
-        const [_, native] = this.nativeListeners.get(key) as [boolean, Deno.Listener];
-        return native;
-    }
-    public keys(): Array<number>
-    {
-        return Array.from(this.nativeListeners.keys());
-    }
-    public close(key?: number): void
-    {
-        if (key && this.nativeListeners.has(key))
-        {
-            const [_, listener] = this.nativeListeners.get(key) as [boolean, Deno.Listener];
-            this.nativeListeners.delete(listener.rid);
-            listener.close();
-        }
-        else
-        {
-            for (const [_1, [_2, listener]] of this.nativeListeners)
-                listener.close();
-            this.nativeListeners.clear();
-        }
-    }
-}
 
 enum StatusCode
 {
