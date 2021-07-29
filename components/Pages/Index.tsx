32ad4aebda692647233932354018f6b6c1998f4a
Ziad Khattab <ziadkh@amazon.com>
2021-07-29 14:47:12 -0400

    Removed outdated GraphQL

--- components/Pages/Index.tsx
+++ components/Pages/Index.tsx
@@ -8,17 +8,6 @@ import Page from "../Page.tsx";
 
 export default function Index()
 {
-    const promise = GraphQL.useGraphQL({ query: graphql`query { request }` });
-    function effect()
-    {
-        async function __effect()
-        {
-            const data = await promise;
-            if (data) Console.log(data);
-        }
-        __effect();
-    }
-    React.useEffect(effect);
     const element: React.ReactElement =
         <Page
             helmet={<title>httpsaurus</title>}
