f4a0f1b621a4788dc0e92b5f4fbb0ccaeab8f983
Ziad Khattab <ziadkh@amazon.com>
2021-07-31 20:23:39 -0400

    Added useRefresh() hook

--- components/App.tsx
+++ components/App.tsx
@@ -2,6 +2,7 @@
 import * as React from "react";
 import * as ReactRouter from "react-router-dom";
 
+import { useRefresh } from "./Core/Core.tsx";
 import { Spinner } from "./Loading.tsx";
 import Index from "./Pages/Index.tsx";
 import MobileProf from "./Pages/MobileProf.tsx";
@@ -16,26 +17,19 @@ interface Props
 }
 export default function App(props: Props)
 {
-    const [loading, setLoading] = React.useState(true);
-    const effect = function ()
-    {
-        const refresh = async function ()
-        {
-            await props.client.fetchRefresh();
-            setLoading(false);
-        };
-        refresh();
-    };
+    const [loading, effect] = useRefresh(props.client.fetchRefresh);
     React.useEffect(effect);
-    if (loading)
-        return <Spinner />;
-    const element =
-        <ReactRouter.Routes>
-            <ReactRouter.Route path="/" element={<Index />} />
-            <ReactRouter.Route path="/mobile-prof" element={<MobileProf />} />
-            <ReactRouter.Route path="/login" element={<Login />} />
-            <ReactRouter.Route path="/register" element={<Register />} />
-            <ReactRouter.Route path="*" element={<Error code={404} text="Not Found" />} />
-        </ReactRouter.Routes>;
-    return element;
+    if (loading()) return <Spinner />;
+    else
+    {
+        const element =
+            <ReactRouter.Routes>
+                <ReactRouter.Route path="/" element={<Index />} />
+                <ReactRouter.Route path="/mobile-prof" element={<MobileProf />} />
+                <ReactRouter.Route path="/login" element={<Login />} />
+                <ReactRouter.Route path="/register" element={<Register />} />
+                <ReactRouter.Route path="*" element={<Error code={404} text="Not Found" />} />
+            </ReactRouter.Routes>;
+        return element;
+    }
 }
--- components/Core/Core.tsx
+++ components/Core/Core.tsx
@@ -2,4 +2,5 @@
 export { Suspense } from "./Suspense.tsx";
 export { environment, Environment } from "./Environment.tsx";
 export { useToken } from "./Token.tsx";
+export { useRefresh } from "./Refresh.tsx";
 export { Console } from "../../client/console.tsx";
