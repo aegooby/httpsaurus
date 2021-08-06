915232c4803e5b3136305049130327c7f21b759a
aegooby <aegooby@gmail.com>
2021-08-05 20:41:12 -0400

    Updated tests

--- tests/test.tsx
+++ tests/test.tsx
@@ -1,67 +1,14 @@
 
-import * as server from "../server/server.tsx";
-import * as async from "@std/async";
+import { Console } from "../server/server.tsx";
 
 try
 {
-    const tests: Deno.TestDefinition[] =
-        [
-            {
-                name: ": run for 5 seconds (HTTP)",
-                async fn(): Promise<void>
-                {
-                    console.log("");
-                    const serverRunOptions: Deno.RunOptions =
-                    {
-                        cmd:
-                            [
-                                "deno", "run", "--unstable", "--allow-all",
-                                "--import-map", "import-map.json", "server/daemon.tsx",
-                                "--hostname", "localhost"
-                            ],
-                        env: { DENO_DIR: ".cache/" }
-                    };
-                    const serverProcess = Deno.run(serverRunOptions);
-                    const time = async.delay(5000);
-                    const status = serverProcess.status();
-                    await time;
-                    serverProcess.close();
-                    serverProcess.kill(Deno.Signal.SIGKILL);
-                    await status;
-                },
-                sanitizeOps: false,
-                sanitizeResources: false,
-            },
-            {
-                name: ": run for 5 seconds (HTTPS)",
-                async fn(): Promise<void>
-                {
-                    console.log("");
-                    const serverRunOptions: Deno.RunOptions =
-                    {
-                        cmd:
-                            [
-                                "deno", "run", "--unstable", "--allow-all",
-                                "--import-map", "import-map.json", "server/daemon.tsx",
-                                "--hostname", "localhost", "--tls", "cert/localhost/"
-                            ],
-                        env: { DENO_DIR: ".cache/" }
-                    };
-                    const serverProcess = Deno.run(serverRunOptions);
-                    const time = async.delay(5000);
-                    const status = serverProcess.status();
-                    await time;
-                    serverProcess.close();
-                    serverProcess.kill(Deno.Signal.SIGKILL);
-                    await status;
-                },
-            },
-        ];
+    const tests: Deno.TestDefinition[] = [];
     for (const test of tests)
         Deno.test(test);
 }
 catch (error)
 {
-    server.Console.error(error.toString());
+    Console.error(error.toString());
     Deno.exit(1);
 }
