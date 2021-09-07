023e5740c32ecdd3a879cfbc5d06f8c3d2df7048
aegooby <aegooby@gmail.com>
2021-09-07 14:05:55 -0400

    Simplified error handling

--- server/turtle/handler.rs
+++ server/turtle/handler.rs
@@ -1,25 +1,52 @@
+use tokio::runtime::Handle;
+
 use super::auth::Token;
 use super::message;
 use super::server;
 
+#[derive(Debug)]
+pub struct HandleError {}
+
+impl std::error::Error for HandleError {}
+impl std::fmt::Display for HandleError {
+    fn fmt(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(formatter, "Error handling request")
+    }
+}
+impl From<jsonwebtoken::errors::Error> for HandleError {
+    fn from(_: jsonwebtoken::errors::Error) -> Self {
+        Self {}
+    }
+}
+impl From<redis::RedisError> for HandleError {
+    fn from(_: redis::RedisError) -> Self {
+        Self {}
+    }
+}
+impl From<json::JsonError> for HandleError {
+    fn from(_: json::JsonError) -> Self {
+        Self {}
+    }
+}
+
 mod auth {
+    use super::HandleError;
+
     use super::message;
     use super::server;
     use super::Token;
     pub async fn handle(
         message: &mut message::Message,
         context: server::Context,
-    ) {
+    ) -> Result<(), HandleError> {
         if let Some(refresh) = message.cookies.get("refresh") {
-            if let Ok(claims) = context.auth.refresh.verify(refresh.to_string())
-            {
-                if let Ok(mut json) = context.redis.json().await {
-                    let path = Some("$".to_string());
-                    if let Ok(result) = json.get(claims.sub, path, None).await {
-                    }
-                }
-            }
+            let claims = context.auth.refresh.verify(refresh.to_string())?;
+            let mut json = context.redis.json().await?;
+            let path = Some("$".to_string());
+            let result = json.get(claims.sub, path, None).await?;
+            let claims_json = json::parse(result.as_str())?;
         }
+        Ok(())
     }
 }
 
